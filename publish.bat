ad21e82c26153e25c9fbf27d90977b6b2d0c33d0
Rangel Reale <rangelspam@gmail.com>
2020-11-06 10:18:46 -0300

    move tests to package publish script

---
+++ publish.bat
@@ -0,0 +1,39 @@
+@echo off
+
+if "%1"=="" goto paramerror
+
+if "%1" NEQ "patch" (
+    if "%1" NEQ "minor" (
+        goto paramerror
+    )
+)
+
+bump2version --verbose %1
+if errorlevel 1 goto error
+
+git push --follow-tags origin
+if errorlevel 1 goto error
+
+rmdir /s/q dist
+if errorlevel 1 goto error
+
+python setup.py sdist bdist_wheel
+if errorlevel 1 goto error
+
+twine upload dist/*
+if errorlevel 1 goto error
+
+goto end
+
+:PARAMERROR
+echo.
+echo Error in input parameters.
+goto end
+
+:ERROR
+echo.
+echo There was an error publishing the package.
+goto end
+
+:END
+echo Finished
--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setuptools.setup(
     url="https://github.com/RangelReale/kg_traefik2",
     packages=setuptools.find_packages(),
     install_requires=INSTALL_REQUIRES,
+    test_suite="kg_traefik2.tests",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
